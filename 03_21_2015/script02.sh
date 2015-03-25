7855fe62271e1c383fcb40109b8e9fbd133a1f74
Laboratório Experimental de Cluster Computing <jlzem@unimep.br>
2015-03-24 22:38:21 -0300

    Create script02.sh

---
+++ 03_21_2015/script02.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "A quantidade de parametros informada foi: $#"
+echo "O parametros informados foram: $*"
+
+echo "O parametro de numero 0 e: ${0}"
+echo "O parametro de numero 1 e: ${1}"
+echo "O parametro de numero 2 e: ${2}"
+echo "O parametro de numero 3 e: ${3}"
